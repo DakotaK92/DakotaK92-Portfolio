a2c86a0deabf5e8703d5c0e4ed92157079eb3102
DakotaK92 <dakotaking92@gmail.com>
2025-04-29 20:49:28 -0400

    small change to code

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ import Stats from "@components/Stats";
 
 export default function Home() {
   return (
-    <section className="h-full">
+    <section className="h-full p-2">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */} 
